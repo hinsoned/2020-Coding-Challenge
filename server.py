1484f3764b80740662f401dbd35b964a5df00fc3
hinsoned <102074110+hinsoned@users.noreply.github.com>
2024-08-19 12:39:18 -0400

    Immediate update of page
    Sorts the scoreboard and updates the page with each button press in real time using ajax and already existing functions.

--- server.py
+++ server.py
@@ -38,6 +38,7 @@ scoreboard = [
 
 @app.route('/')
 def show_scoreboard():
+    #passes in the scoreboard variable that contains the list of teams
     return render_template('scoreboard.html', scoreboard = scoreboard) 
 
 @app.route('/increase_score', methods=['GET', 'POST'])
@@ -51,11 +52,14 @@ def increase_score():
         if team["id"] == team_id:
             team["score"] += 1
 
+    #sort the scoreboard object here using the score field
+    scoreboard.sort(key=lambda x: x["score"], reverse=True)
+
     return jsonify(scoreboard=scoreboard)
 
 
 if __name__ == '__main__':
-   app.run(debug = True)
+   app.run(debug = True, use_reloader = False)
 
 
 
--- static/scoreboard.js
+++ static/scoreboard.js
@@ -1,11 +1,15 @@
 function display_scoreboard(scoreboard){
+  // this empty() function empties the div with id "teams". This does work needed when
+  //updating the page with each button press
   $("#teams").empty();
+  // this for each loop iterates through the list of teams in scoreboard and runs addTeamView for each team
   $.each(scoreboard, function(index, team){
     addTeamView(team.id, team.name, team.score);
   });
 }
 
 function addTeamView(id, name, score){
+  // each time addTeamView is called it creates this HTML to add to the DOM
   var team_template = $("<div class = row></div>");
   var name_template = $("<div class = col-md-5></div>");
   var score_template = $("<div class = col-md-2></div>");
@@ -31,8 +35,14 @@ function increase_score(id){
     dataType : "json",
     contentType: "application/json; charset=utf-8",
     data : JSON.stringify(team_id),
+    //I am used to fetch, but I assume this is where the change to the DOM is made
+    //This gets the entire scoreboard object back from the server
+    //I need to loop through it to update the entire scoreboard
+    //It is sorted by the server so I think I can just call display_scoreboard
     success: function(result){
-        
+      //This confused me at first but it has to be result.scoreboard because the 
+      //result object is the scoreboard, not the list of teams
+      display_scoreboard(result.scoreboard);
     },
     error: function(request, status, error){
         console.log("Error");
@@ -43,6 +53,8 @@ function increase_score(id){
   });
 }
 
+//This is what actually calls display_scoreboard to display the scoreboard and buttons.
+//Once the DOM is ready, it will call display_scoreboard
 $(document).ready(function(){
   display_scoreboard(scoreboard);
 })
